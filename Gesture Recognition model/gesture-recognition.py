cd64a58678434553f6d1ccb937b5c2b535eb710a
endertuber123 <endertuber123@gmail.com>
2023-04-06 16:04:42 -0500

    Added OSC client to AI model.
    The gesture-recoginition.py should now send OSC messages whenever the detected gesture changes.

--- Gesture Recognition model/gesture-recognition.py
+++ Gesture Recognition model/gesture-recognition.py
@@ -4,17 +4,45 @@ from keras.models import load_model
 import cv2
 import numpy as np
 from random import choice
+from pythonosc.udp_client import SimpleUDPClient
+
+OSC_IP = "127.0.0.1"
+
+OSC_PORT = "1115"
+
+OSC_ADDRESS_PREFIX = "/GESTURE/set/"
+
+MODEL_PATH = "Gesture Recognition model\model.h5"
 
 REV_CLASS_MAP = {
     0: "gesture1",
     1: "nothing",
 }
 
+#TROY: The lookup table is formatted as a dictionary of dictionaries. Only the effects you want edited should be included.
+GESTURE_LOOKUP_TABLE = {
+    "gesture1": {
+        "-REVERB-": 25
+    }, "nothing": {
+        "-REVERB-": 0
+    }
+}
+
+osc_client = SimpleUDPClient(OSC_IP, int(OSC_PORT))
+
+#TROY: This method checks the given gesture name against the lookup table, which is then used to send OSC messages. An exception is raised if a gesture name is supplied that isn't in the lookup table.
+def osc_gesture_sender(gesture):
+    global osc_client
+    if GESTURE_LOOKUP_TABLE.get(gesture):
+        for effect in GESTURE_LOOKUP_TABLE[gesture]:
+            osc_client.send_message(OSC_ADDRESS_PREFIX + effect, GESTURE_LOOKUP_TABLE[gesture][effect])
+    else:
+        raise KeyError(gesture + " is not in the lookup table!")
 
 def mapper(val):
     return REV_CLASS_MAP[val]
 
-model = load_model("model.h5")
+model = load_model(MODEL_PATH)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
@@ -53,6 +81,11 @@ while True:
                 #(330, 50), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
     cv2.putText(frame, "Gesture: " + gesture_name,
                 (100, 450), font, 2, (0, 255, 0), 4, cv2.LINE_AA)
+
+    #TROY: Branch is here to prevent spamming the same gesture.
+    if gesture_name != prev_move:
+        osc_gesture_sender(gesture_name)
+        prev_move = gesture_name
 
     #if computer_move_name != "nothing":
         #icon = cv2.imread(
